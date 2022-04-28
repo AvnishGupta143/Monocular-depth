-Fixed the semantic gt dataset loader with proper # of channels and correct datatype and scaling -Training now prints individual losses for semantic and depth losses -Training seems to be working well (in progress) -Changed the semantic decoder to have final layers of 64 filters (instead of down to 32 and 16 first) -Added a softmax to segmentation output for test time

monodepth/monodepth_dataloader.py
@@ -45,8 +45,7 @@ class MonodepthDataloader(object):
             semantic_image_path = tf.string_join([self.data_path, split_line[2]])
             left_image_o  = self.read_image(left_image_path)
             right_image_o = self.read_image(right_image_path)
-            # we only need 1 channel of semantic (they're all same), but need to keep 3 dim tensor still
-            semantic_image_o = self.read_image(semantic_image_path)[:, :, 0:1]
+            semantic_image_o = self.read_semantic_image(semantic_image_path)
 
         if mode == 'train':
             # randomly flip images
@@ -77,8 +76,8 @@ class MonodepthDataloader(object):
                 self.right_image_batch = tf.stack([right_image_o,  tf.image.flip_left_right(right_image_o)],  0)
                 self.right_image_batch.set_shape( [2, None, None, 3])
 
-            self.semantic_image_batch = tf.stack([semantic_image_o],  0)
-            self.semantic_image_batch.set_shape( [1, None, None, 3])
+            self.semantic_image_batch = tf.stack([semantic_image_o, tf.image.flip_left_right(semantic_image_o)],  0)
+            self.semantic_image_batch.set_shape( [1, None, None, 1])
 
     def augment_image_pair(self, left_image, right_image):
         # randomly shift gamma
@@ -120,5 +119,28 @@ class MonodepthDataloader(object):
 
         image  = tf.image.convert_image_dtype(image,  tf.float32)
         image  = tf.image.resize_images(image,  [self.params.height, self.params.width], tf.image.ResizeMethod.AREA)
+
+        return image
+
+    def read_semantic_image(self, image_path):
+        # tf.decode_image does not return the image size, this is an ugly workaround to handle both jpeg and png
+        path_length = string_length_tf(image_path)[0]
+        file_extension = tf.substr(image_path, path_length - 3, 3)
+        file_cond = tf.equal(file_extension, 'jpg')
+
+        image = tf.cond(file_cond, lambda: tf.image.decode_jpeg(tf.read_file(image_path)),
+                        lambda: tf.image.decode_png(tf.read_file(image_path)))
+
+        # if the dataset is cityscapes, we crop the last fifth to remove the car hood
+        if self.dataset == 'cityscapes':
+            o_height = tf.shape(image)[0]
+            crop_height = (o_height * 4) // 5
+            image = image[:crop_height, :, :]
+
+        # image = tf.image.convert_image_dtype(image, tf.int64)
+        # we only need 1 channel of semantic (they're all same), but need to keep 3 dim tensor still
+        # semantic map is NOT float, it is int
+        # semantic image also not suited for resizing using any form of blend/average/area, since int classes
+        image = tf.image.resize_images(image, [self.params.height, self.params.width], tf.image.ResizeMethod.NEAREST_NEIGHBOR)[:, :, 0:1]
 
         return image

monodepth/monodepth_main.py
@@ -35,8 +35,8 @@ parser.add_argument('--data_path',                 type=str,   help='path to the
 parser.add_argument('--filenames_file',            type=str,   help='path to the filenames text file', required=True)
 parser.add_argument('--input_height',              type=int,   help='input height', default=128)
 parser.add_argument('--input_width',               type=int,   help='input width', default=256)
-parser.add_argument('--batch_size',                type=int,   help='batch size', default=1)
-parser.add_argument('--num_epochs',                type=int,   help='number of epochs', default=50)
+parser.add_argument('--batch_size',                type=int,   help='batch size', default=8)
+parser.add_argument('--num_epochs',                type=int,   help='number of epochs', default=200)
 parser.add_argument('--learning_rate',             type=float, help='initial learning rate', default=1e-4)
 parser.add_argument('--lr_loss_weight',            type=float, help='left-right consistency weight', default=1.0)
 parser.add_argument('--alpha_image_loss',          type=float, help='weight between SSIM and L1 in the image loss', default=0.85)
@@ -69,6 +69,28 @@ def count_text_lines(file_path):
     lines = f.readlines()
     f.close()
     return len(lines)
+
+# Credit to: https://gist.github.com/iganichev/d2d8a0b1abc6b15d4a07de83171163d4
+def optimistic_restore(save_file):
+    reader = tf.train.NewCheckpointReader(save_file)
+    saved_shapes = reader.get_variable_to_shape_map()
+    var_names = sorted([(var.name, var.name.split(':')[0]) for
+                        var in tf.global_variables()
+                        if var.name.split(':')[0] in saved_shapes])
+    restore_vars = []
+    name2var = dict(zip(map(lambda x: x.name.split(':')[0],
+                            tf.global_variables()),
+                        tf.global_variables()))
+    with tf.variable_scope('', reuse=True):
+        for var_name, saved_var_name in var_names:
+            curr_var = name2var[saved_var_name]
+            var_shape = curr_var.get_shape().as_list()
+            if var_shape == saved_shapes[saved_var_name]:
+                restore_vars.append(curr_var)
+
+    saver = tf.train.Saver(restore_vars)
+    return saver
+
 
 def train(params):
     """Training loop."""
@@ -105,15 +127,23 @@ def train(params):
 
         tower_grads  = []
         tower_losses = []
+        depth_losses = []
+        seg_losses = []
         reuse_variables = None
         with tf.variable_scope(tf.get_variable_scope()):
             for i in range(args.num_gpus):
-                with tf.device('/gpu:%d' % i):
+                with tf.device('/cpu:%d' % i):
 
                     model = MonodepthModel(params, args.mode, left_splits[i], right_splits[i], semantic_splits[i], reuse_variables, i)
 
                     loss = model.total_loss
+
                     tower_losses.append(loss)
+
+                    depth_loss = model.subtotal_depth_losses
+                    seg_loss = model.seg_loss
+                    depth_losses.append(depth_loss)
+                    seg_losses.append(seg_loss)
 
                     reuse_variables = True
 
@@ -126,9 +156,13 @@ def train(params):
         apply_gradient_op = opt_step.apply_gradients(grads, global_step=global_step)
 
         total_loss = tf.reduce_mean(tower_losses)
+        depth_loss_sub = tf.reduce_mean(depth_losses)
+        seg_loss_sub = tf.reduce_mean(seg_losses)
 
         tf.summary.scalar('learning_rate', learning_rate, ['model_0'])
         tf.summary.scalar('total_loss', total_loss, ['model_0'])
+        tf.summary.scalar('depth_loss_sub', depth_loss_sub, ['model_0'])
+        tf.summary.scalar('seg_loss_sub', seg_loss_sub, ['model_0'])
         summary_op = tf.summary.merge_all('model_0')
 
         # SESSION
@@ -153,24 +187,29 @@ def train(params):
 
         # LOAD CHECKPOINT IF SET
         if args.checkpoint_path != '':
+            #train_saver = optimistic_restore(args.checkpoint_path.split(".")[0])
             train_saver.restore(sess, args.checkpoint_path.split(".")[0])
 
             if args.retrain:
                 sess.run(global_step.assign(0))
+
+        #else:
+        #    train_saver = tf.train.Saver()
 
         # GO!
         start_step = global_step.eval(session=sess)
         start_time = time.time()
         for step in range(start_step, num_total_steps):
             before_op_time = time.time()
-            _, loss_value = sess.run([apply_gradient_op, total_loss])
+            _, loss_value, d_loss, s_loss = sess.run([apply_gradient_op, total_loss, depth_loss_sub, seg_loss_sub])
             duration = time.time() - before_op_time
             if step and step % 100 == 0:
                 examples_per_sec = params.batch_size / duration
                 time_sofar = (time.time() - start_time) / 3600
                 training_time_left = (num_total_steps / step - 1.0) * time_sofar
                 print_string = 'batch {:>6} | examples/s: {:4.2f} | loss: {:.5f} | time elapsed: {:.2f}h | time left: {:.2f}h'
-                print(print_string.format(step, examples_per_sec, loss_value, time_sofar, training_time_left))
+                print(print_string.format(step, examples_per_sec, loss_value, time_sofar, training_time_left), end="")
+                print("  depth_loss: {}, sg_loss: {}".format(d_loss, s_loss))
                 summary_str = sess.run(summary_op)
                 summary_writer.add_summary(summary_str, global_step=step)
             if step and step % 10000 == 0:

monodepth/monodepth_model.py
@@ -46,7 +46,7 @@ class MonodepthModel(object):
         self.left = left
         self.right = right
         self.segmented = segmented
-        self.NUM_SEMANTIC_CLASSES = 32  # TODO: pass this in?
+        self.NUM_SEMANTIC_CLASSES = 34  # TODO: pass this in?
         self.model_collection = ['model_' + str(model_index)]
 
         self.reuse_variables = reuse_variables
@@ -261,20 +261,22 @@ class MonodepthModel(object):
             # self.disp3_semantic = self.get_disp(iconv3_semantic)  # Removed disp section from semantic
             # udisp3_semantic  = self.upsample_nn(self.disp3_semantic, 2)  # Removed disp section from semantic
 
-            upconv2_semantic = upconv(iconv3_semantic, 32, 3, 2)  # H/2
+            upconv2_semantic = upconv(iconv3_semantic, 64, 3, 2)  # H/2
             # concat2_semantic = tf.concat([upconv2_semantic, skip1, udisp3_semantic], 3)  # Removed disp section from semantic
             concat2_semantic = tf.concat([upconv2_semantic, skip1], 3)
-            iconv2_semantic = conv(concat2_semantic, 32, 3, 1)
+            iconv2_semantic = conv(concat2_semantic, 64, 3, 1)
             # self.disp2_semantic = self.get_disp(iconv2_semantic)  # Removed disp section from semantic
             # udisp2_semantic  = self.upsample_nn(self.disp2_semantic, 2)  # Removed disp section from semantic
 
-            upconv1_semantic = upconv(iconv2_semantic, 16, 3, 2)  # H
+            upconv1_semantic = upconv(iconv2_semantic, 64, 3, 2)  # H
             # concat1_semantic = tf.concat([upconv1_semantic, udisp2_semantic], 3)  # Removed disp section from semantic
             # concat1_semantic = tf.concat([upconv1_semantic], 3)
-            iconv1_semantic = conv(upconv1_semantic, self.NUM_SEMANTIC_CLASSES, 1, 1, activation_fn=tf.nn.softmax)
+            iconv1_semantic = conv(upconv1_semantic, self.NUM_SEMANTIC_CLASSES, 1, 1, activation_fn=None)
             # self.disp1_semantic = self.get_disp(iconv1_semantic)  # Removed disp section from semantic
 
-            self.seg_decoder_out = iconv1_semantic
+            self.seg_decoder_preactivation_out = iconv1_semantic
+
+            self.semantic_softmax_out = tf.nn.softmax(self.seg_decoder_preactivation_out)
 
     def build_resnet50(self):
         # set convenience functions
@@ -356,19 +358,19 @@ class MonodepthModel(object):
                 # self.disp3_semantic = self.get_disp(iconv3_semantic)  # Removed disp section from semantic
                 # udisp3_semantic = self.upsample_nn(self.disp3_semantic, 2)  # Removed disp section from semantic
 
-                upconv2_semantic = upconv(iconv3_semantic, 32, 3, 2)  # H/2
+                upconv2_semantic = upconv(iconv3_semantic, 64, 3, 2)  # H/2
                 # concat2_semantic = tf.concat([upconv2_semantic, skip1, udisp3_semantic], 3)  # Removed disp section from semantic
                 concat2_semantic = tf.concat([upconv2_semantic, skip1], 3)
-                iconv2_semantic = conv(concat2_semantic, 32, 3, 1)
+                iconv2_semantic = conv(concat2_semantic, 64, 3, 1)
                 # self.disp2_semantic = self.get_disp(iconv2_semantic)  # Removed disp section from semantic
                 # udisp2_semantic = self.upsample_nn(self.disp2_semantic, 2)  # Removed disp section from semantic
 
-                upconv1_semantic = upconv(iconv2_semantic, 16, 3, 2)  # H
+                upconv1_semantic = upconv(iconv2_semantic, 64, 3, 2)  # H
                 # concat1_semantic = tf.concat([upconv1_semantic, udisp2_semantic], 3)  # Removed disp section from semantic
                 concat1_semantic = tf.concat([upconv1_semantic], 3)
-                iconv1_semantic = conv(concat1_semantic, 16, 3, 1)
+                iconv1_semantic = conv(concat1_semantic, 64, 3, 1)
                 # self.disp1_semantic = self.get_disp(iconv1_semantic)
-                self.seg_decoder_out = iconv1_semantic
+                self.seg_decoder_preactivation_out = iconv1_semantic
 
     def build_model(self):
         with slim.arg_scope([slim.conv2d, slim.conv2d_transpose], activation_fn=tf.nn.elu):
@@ -399,10 +401,10 @@ class MonodepthModel(object):
             self.disp_right_est = [tf.expand_dims(d[:, :, :, 1], 3) for d in self.disp_est]
 
         with tf.variable_scope('segmentation'):
-            self.segmentation = self.seg_decoder_out
+            self.segmentation = self.semantic_softmax_out
 
         with tf.variable_scope('dual_output'):
-            self.dual_output = [self.disp_left_est[0], self.segmentation]
+            self.dual_output = [self.disp_left_est[0], self.seg_decoder_preactivation_out]
 
         if self.mode == 'test':
             return
@@ -462,14 +464,17 @@ class MonodepthModel(object):
 
             # (ADDED) Segmentation LOSS
             # TODO: finish this and check it
-            self.seg_loss = tf.losses.sparse_softmax_cross_entropy(self.segmented, self.seg_decoder_out)
+            self.seg_loss = tf.losses.sparse_softmax_cross_entropy(self.segmented, self.seg_decoder_preactivation_out)
+
+            # TODO: adjust multipliers for losses to proper scales
+            SEGMENTATION_LOSS_SCALER = 5
+            DEPTH_LOSS_SCALER = 1
 
             # TOTAL LOSS
             self.subtotal_depth_losses = self.image_loss + self.params.disp_gradient_loss_weight * self.disp_gradient_loss + self.params.lr_loss_weight * self.lr_loss
-            # TODO: adjust multipliers for losses to proper scales
-            SEGMENTATION_LOSS_SCALER = 1
-            DEPTH_LOSS_SCALER = 1
-            self.total_loss = self.subtotal_depth_losses * DEPTH_LOSS_SCALER + self.seg_loss * SEGMENTATION_LOSS_SCALER
+            self.subtotal_depth_losses *= DEPTH_LOSS_SCALER
+            self.seg_loss *= SEGMENTATION_LOSS_SCALER
+            self.total_loss = self.subtotal_depth_losses + self.seg_loss
 
     def build_summaries(self):
         # SUMMARIES

monodepth/monodepth_simple.py
@@ -65,8 +65,8 @@ def test_simple(params):
     """Test function."""
 
     left  = tf.placeholder(tf.float32, [2, args.input_height, args.input_width, 3])
-    segmented  = tf.placeholder(tf.float32, [1, args.input_height, args.input_width, 3])
-    model = MonodepthModel(params, "test", left, None, segmented)
+    # segmented  = tf.placeholder(tf.float32, [1, args.input_height, args.input_width, 3])
+    model = MonodepthModel(params, "test", left, None, None)
 
     input_image = scipy.misc.imread(args.image_path, mode="RGB")
     original_height, original_width, num_channels = input_image.shape
@@ -115,17 +115,18 @@ def test_simple(params):
 
     # train_saver.restore(sess, restore_path)
 
-    disp = sess.run(model.disp_left_est[0], feed_dict={left: input_images})
-    # disp, seg = sess.run(model.dual_output, feed_dict={left: input_images, segmented: input_images})
+    # disp = sess.run(model.disp_left_est[0], feed_dict={left: input_images})
+    disp, seg = sess.run(model.dual_output, feed_dict={left: input_images})
 
     disp_pp = post_process_disparity(disp.squeeze()).astype(np.float32)
 
     output_directory = os.path.dirname(args.image_path)
     output_name = os.path.splitext(os.path.basename(args.image_path))[0]
 
-    np.save(os.path.join(output_directory, "{}_disp.npy".format(output_name)), disp_pp)
+    np.save(os.path.join(output_directory, "{}_disp_seg.npy".format(output_name)), disp_pp)
     disp_to_img = scipy.misc.imresize(disp_pp.squeeze(), [original_height, original_width])
-    plt.imsave(os.path.join(output_directory, "{}_disp.png".format(output_name)), disp_to_img, cmap='plasma')
+    plt.imsave(os.path.join(output_directory, "{}_disp_seg_apr27.png".format(output_name)), disp_to_img, cmap='plasma')
+    plt.imsave(os.path.join(output_directory, "{}_seg_seg_apr27.png".format(output_name)), seg[0].argmax(axis=2), cmap='plasma')
 
     print('done!')
 